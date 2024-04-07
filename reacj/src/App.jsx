3007896557192c82ece35b44397bf68d81df7053
DiyawanDawan <diyawan0@gmail.com>
2024-04-07 10:54:03 +0800

    Learn Componen

--- reacj/src/App.jsx
+++ reacj/src/App.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 
 
+// Craate Class Componen
 class Button extends React.Component {
   render() {
     return (
-      <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700  bg-slate-800 text-neutral-50">
+      <button type="button" className="-m-2.5 rounded-md p-2.5 bg-amber-900  text-slite-500 text-neutral-50">
         Close menu
       </button>
     )
   }
 }
 
-function ButtonAmber() {
-  return (
-    <button type="button" className="-m-2.5 rounded-md p-2.5 bg-amber-500  text-red-500 text-neutral-50">
-      Close menu
-    </button>
-  )
-}
-
-const ButtonSlite = () => {
+// Function Compoenen
+function ButtonRen() {
   return (
     <button type="button" className="-m-2.5 rounded-md p-2.5 bg-amber-900  text-slite-500 text-neutral-50">
       Close menu
@@ -27,13 +21,23 @@ const ButtonSlite = () => {
   )
 }
 
+// Arrow Function Componen
+const ButtonArrow = () => {
+  return (
+    <button type="button" className="-m-2.5 rounded-md p-2.5 bg-amber-900  text-slite-500 text-neutral-50">
+      Close menu
+    </button>
+  )
+}
+
+// Function  Componen
 function App() {
   return (
     <div className="flex justify-center text-tahiti min-h-screen items-center bg-lime-100">
       <div className="flex gap-x-8">
         <Button />
-        <ButtonAmber />
-        <ButtonSlite />
+        <ButtonRen />
+        <ButtonArrow />
       </div>
     </div>
   );
