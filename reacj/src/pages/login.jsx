fc96170165ff38c781c4ba26ccaf721b7b73440c
DiyawanDawan <diyawan0@gmail.com>
2024-04-08 11:47:51 +0800

    Laern Condition Rendering Ternary operaror and if else operator

--- reacj/src/pages/login.jsx
+++ reacj/src/pages/login.jsx
@@ -1,17 +1,11 @@
-import { Link } from "react-router-dom";
 import FormLogin from "../components/Fragments/FormLogin";
 import AoutLayout from "../components/Layouts/Auth/OutsLayout";
 
 const LoginPage = () => {
   return (
-    <AoutLayout titel="LogIn">
+    <AoutLayout titel="LogIn" type="Login">
       <FormLogin />
-      <p className="mt-3 py-3 text-sm text-center">
-        Dont&apos;t an have account? {" "}
-        <Link className="text-blue-700 text-underline" to="/register">
-          Sign Up
-        </Link>
-      </p>
+     
     </AoutLayout>
   );
 };
--- reacj/src/pages/register.jsx
+++ reacj/src/pages/register.jsx
@@ -1,17 +1,10 @@
-import { Link } from "react-router-dom"
 import FormRegister from "../components/Fragments/FormRegister"
 import AoutLayout from "../components/Layouts/Auth/OutsLayout"
 
 const RegisterPage = () => {
     return (
-        <AoutLayout titel="Register">
+        <AoutLayout titel="Register" type="Register">
             <FormRegister/>
-            <p className="mt-3 py-3 text-sm text-center">
-                Have an account? {" "}
-                <Link className="text-blue-700 text-underline" to="/login">
-                Login
-                </Link>
-            </p>
         </AoutLayout>
     )
 }
